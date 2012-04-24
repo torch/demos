fd6f91ee868020ded006627082ae2b967cf46f23
Aysegul Dundar <adundar@purdue.edu>
2012-04-24 09:53:57 -0400

    Attention runs on the computer

--- attention/attention.lua
+++ attention/attention.lua
@@ -1,6 +1,6 @@
 --#!/usr/bin/env qlua
 ------------------------------------------------------------
--- e. culurciello
+-- Author: Aysegul Dundar
 -- temporal difference simulator
 
 --require 'XLearn'
