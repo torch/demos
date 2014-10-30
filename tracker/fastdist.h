0005e0d69921bdb9ec87dc3005066f9514447041
Soumith Chintala <soumith@fb.com>
2014-10-30 13:53:48 -0400

    removing non-working demos

--- tracker/fastdist.h
+++
@@ -1,8 +0,0 @@
-int RBF(float *input, int ichannels, int iheight, int iwidth, 
-   float *output, int numProto, float *code, float *weight, float *std);
-
-int exponential(float *input, int ichannels, int iheight, int iwidth, 
-   float *output, int numProto, float *code, float *weight, float *std);
-
-int SMR(float *input, int ichannels, int iheight, int iwidth, 
-   float *output, int numProto, float *code, float *weight, float th);
