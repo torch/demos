2762718242a8fbc60343b0d5c1e7743f85e45190
Tommy Unger <tunger10@gmail.com>
2016-06-15 14:23:15 -0400

    Fix padding on first convolutional pooling layer.

--- train-a-digit-classifier/train-on-mnist.lua
+++ train-a-digit-classifier/train-on-mnist.lua
@@ -80,14 +80,14 @@ if opt.network == '' then
       -- stage 1 : mean suppresion -> filter bank -> squashing -> max pooling
       model:add(nn.SpatialConvolutionMM(1, 32, 5, 5))
       model:add(nn.Tanh())
-      model:add(nn.SpatialMaxPooling(3, 3, 3, 3))
+      model:add(nn.SpatialMaxPooling(3, 3, 3, 3, 1, 1))
       -- stage 2 : mean suppresion -> filter bank -> squashing -> max pooling
       model:add(nn.SpatialConvolutionMM(32, 64, 5, 5))
       model:add(nn.Tanh())
       model:add(nn.SpatialMaxPooling(2, 2, 2, 2))
       -- stage 3 : standard 2-layer MLP:
-      model:add(nn.Reshape(64*2*2))
-      model:add(nn.Linear(64*2*2, 200))
+      model:add(nn.Reshape(64*3*3))
+      model:add(nn.Linear(64*3*3, 200))
       model:add(nn.Tanh())
       model:add(nn.Linear(200, #classes))
       ------------------------------------------------------------
