d12a0c42a6561c715aa1e616f3e70b4f50d269ce
Maciej <mbajkowski@hotmail.com>
2016-07-20 23:06:48 -0500

    removed cunn dependencies ffrom cpu demo
    Looks like this should be a CPU tests. But when run on a CPU system it would fail with a no CUDA-capable device is detected 38 error because of the cunn dependencies, the above changes seem to fix that problem.

--- profiling/linear-cpu.lua
+++ profiling/linear-cpu.lua
@@ -1,9 +1,10 @@
 
 require 'sys'
-require 'cunn'
+require 'nnx'
+--require 'cunn'
 
-cutorch.setDevice(arg[1] or 1)
-print('DEVID = ' .. cutorch.getDevice())
+--cutorch.setDevice(arg[1] or 1)
+--print('DEVID = ' .. cutorch.getDevice())
 
 bs = 512
 ninputs = 4096
@@ -28,14 +29,13 @@ N=5
 -- pre-alloc states:
 n:forward(i)
 n:backward(i, n.output)
-cutorch.synchronize()
+--cutorch.synchronize()
 
 sys.tic()
 for t = 1,N do
    n:forward(i)
    n:backward(i, n.output)
 end
-cutorch.synchronize()
+--cutorch.synchronize()
 t = sys.toc()/N
 print('Fprop+Bprop+Acc - GFLOP/s:', ops/t/1e9)
-
